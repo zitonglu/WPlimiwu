38e7eff1a2cdd43d6f88c311e3f84edbde6306a3
my <910109610@qq.com>
2019-09-08 09:16:25 +0800

    css five

--- index.php
+++ index.php
@@ -8,32 +8,66 @@
 	<div class="list">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-3 col-sm-4 col-xs-12 item">
+				<div class="col-lg-wu1 col-md-3 col-sm-4 col-xs-6 item">
 					<div class="thumbnail">
 						<a href="#"><img src="https://img.zcool.cn/community/019edb5d71ddd6a801202f17f11844.jpg@260w_195h_1c_1e_1o_100sh.jpg" alt="..."></a>
 						<div class="caption">
 							<a href="1" class="title"><h3>Thumbnail label</h3></a>
-							<a href="#"><p class="description">峻峰史丹利放计算的柯乐芙就……</p></a>
-							<p class="tags"><i class="glyphicon glyphicon-time" aria-hidden="true"></i>2019-9-7 <i class="glyphicon glyphicon-tags" aria-hidden="true"></i> <a href="54">博阿帕奇</a> <a href="54">标签</a></p>
+							<a href="#" class="hidden-xs"><p class="description">峻峰史丹利放计算的柯乐芙就……</p></a>
+							<p class="tags text-right"><i class="glyphicon glyphicon-time" aria-hidden="true"></i>2019-9-7 <span class="hidden-xs"><i class="glyphicon glyphicon-tags" aria-hidden="true"></i> <a href="54">博阿帕奇</a> <a href="54">标签</a></span></p>
 						</div>
 					</div>
-				</div>
-				<div class="col-md-3 col-sm-4 col-xs-12 item">
+				</div><!-- item end -->
+				<div class="col-lg-wu1 col-md-3 col-sm-4 col-xs-6 item">
 					<div class="thumbnail">
-						<img src="http://image.limiwu.com/2019/2019/08/201908295462_7493.jpg" alt="...">
+						<a href="#"><img src="http://image.limiwu.com/2019/2019/08/201908295462_7493.jpg" alt="..."></a>
 						<div class="caption">
-							<h3>很艰苦受理点估计离开时代峻峰史丹利放计算的柯乐芙就</h3>
-							<p>...</p>
+							<a href="1" class="title"><h3>Thumbnail label</h3></a>
+							<a href="#" class="hidden-xs"><p class="description">峻峰史丹利放计算的柯乐芙就……</p></a>
+							<p class="tags text-right"><i class="glyphicon glyphicon-time" aria-hidden="true"></i>2019-9-7 <span class="hidden-xs"><i class="glyphicon glyphicon-tags" aria-hidden="true"></i> <a href="54">博阿帕奇</a> <a href="54">标签</a></span></p>
 						</div>
 					</div>
 				</div>
-				<div class="col-md-3 col-sm-4 col-xs-12">...</div>
-				<div class="col-md-3 col-sm-4 col-xs-12">...</div>
-				<div class="col-md-3 col-sm-4 col-xs-12">...</div>
-				<div class="col-md-3 col-sm-4 col-xs-12">...</div>
-				<div class="col-md-3 col-sm-4 col-xs-12">...</div>
-				<div class="col-md-3 col-sm-4 col-xs-12">...</div>
-				<div class="col-md-3 col-sm-4 col-xs-12">...</div>
+				<div class="col-lg-wu1 col-md-3 col-sm-4 col-xs-6 item">
+					<div class="thumbnail">
+						<a href="#"><img src="http://www.ewceo.com/zb_users/upload/2019/07/201907031562127556790452.jpg" alt="..."></a>
+						<div class="caption">
+							<a href="1" class="title"><h3>Thumbnail label</h3></a>
+							<a href="#" class="hidden-xs"><p class="description">峻峰史丹利放计算的柯乐芙就……</p></a>
+							<p class="tags text-right"><i class="glyphicon glyphicon-time" aria-hidden="true"></i>2019-9-7 <span class="hidden-xs"><i class="glyphicon glyphicon-tags" aria-hidden="true"></i> <a href="54">博阿帕奇</a> <a href="54">标签</a></span></p>
+						</div>
+					</div>
+				</div><!-- item end -->
+				<div class="col-lg-wu1 col-md-3 col-sm-4 col-xs-6 item">
+					<div class="thumbnail">
+						<a href="#"><img src="https://img.zcool.cn/community/019edb5d71ddd6a801202f17f11844.jpg@260w_195h_1c_1e_1o_100sh.jpg" alt="..."></a>
+						<div class="caption">
+							<a href="1" class="title"><h3>Thumbnail label</h3></a>
+							<a href="#" class="hidden-xs"><p class="description">峻峰史丹利放计算的柯乐芙就……</p></a>
+							<p class="tags text-right"><i class="glyphicon glyphicon-time" aria-hidden="true"></i>2019-9-7 <span class="hidden-xs"><i class="glyphicon glyphicon-tags" aria-hidden="true"></i> <a href="54">博阿帕奇</a> <a href="54">标签</a></span></p>
+						</div>
+					</div>
+				</div><!-- item end -->
+				<div class="col-lg-wu1 col-md-3 col-sm-4 col-xs-6 item">
+					<div class="thumbnail">
+						<a href="#"><img src="http://www.ewceo.com/zb_users/upload/2019/01/201901301548831064712430.jpeg" alt="..."></a>
+						<div class="caption">
+							<a href="1" class="title"><h3>Thumbnail label</h3></a>
+							<a href="#" class="hidden-xs"><p class="description">峻峰史丹利放计算的柯乐芙就……</p></a>
+							<p class="tags text-right"><i class="glyphicon glyphicon-time" aria-hidden="true"></i>2019-9-7 <span class="hidden-xs"><i class="glyphicon glyphicon-tags" aria-hidden="true"></i> <a href="54">博阿帕奇</a> <a href="54">标签</a></span></p>
+						</div>
+					</div>
+				</div><!-- item end -->
+				<div class="col-lg-wu1 col-md-3 col-sm-4 col-xs-6 item">
+					<div class="thumbnail">
+						<a href="#"><img src="http://www.ewceo.com/zb_users/upload/2019/02/201902161550292338477218.jpg" alt="..."></a>
+						<div class="caption">
+							<a href="1" class="title"><h3>Thumbnail label</h3></a>
+							<a href="#" class="hidden-xs"><p class="description">峻峰史丹利放计算的柯乐芙就……</p></a>
+							<p class="tags text-right"><i class="glyphicon glyphicon-time" aria-hidden="true"></i>2019-9-7 <span class="hidden-xs"><i class="glyphicon glyphicon-tags" aria-hidden="true"></i> <a href="54">博阿帕奇</a> <a href="54">标签</a></span></p>
+						</div>
+					</div>
+				</div><!-- item end -->
 			</div>
 		</div>
 	</div>
