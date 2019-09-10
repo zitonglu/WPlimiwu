457106ee2e22619cd8615eb17523c5c865755257
xie <910109610@qq.com>
2019-09-10 17:58:46 +0800

    limiwu_first_img()

--- functions.php
+++ functions.php
@@ -8,9 +8,20 @@
  * @since 1.0.0
  */
 
-/**
- * 
- */
+// 获取文章第一张缩略图 
+function limiwu_first_img() {
+	global $post;
+	$first_img = '';
+	ob_start();
+	ob_end_clean();
+	$output = preg_match_all('/<img*.+src=[\'"]([^\'"]+)[\'"].*>/iU', wp_unslash($post->post_content), $matches);
+	if(empty($output)){ 
+		$first_img = get_template_directory_uri().'/image/coffee.jpg';
+	}else {
+		$first_img = $matches [1][0];
+	}
+	return $first_img;
+}
 
 // 开启缩略图功能
 add_theme_support( 'post-thumbnails' );
