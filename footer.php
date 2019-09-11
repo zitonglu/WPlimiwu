c93f3f6acf3ca0517f6c4d2bf2051ed17636529c
xie <910109610@qq.com>
2019-09-11 16:51:36 +0800

    infinitescroll

--- footer.php
+++ footer.php
@@ -1,23 +1,49 @@
+<footer class="container">
+    <p>这里放一些友情链接之类的东西，需要把整个界面撑起来，起码50px。</p>
+</footer>
 <!-- JQ and Bootstrap JS -->
 <script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
 <script src="http://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <!-- <script src="<?php bloginfo('template_url'); ?>/js/jquery.min.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script> -->
+<?php if( is_single() || is_page()):?>
+<?php else:?>
 <!-- 瀑布流 JS -->
 <script src="<?php bloginfo('template_url'); ?>/js/masonry.min.js"></script>
+<!-- 无限下拉 JS -->
+<script src="<?php bloginfo('template_url'); ?>/js/infinitescroll.min.js"></script>
 <!-- 加载图片 JS -->
 <script src="<?php bloginfo('template_url'); ?>/js/imagesloaded.min.js"></script>
-<script>
-    $(function() {
-        var masonryNode = $('#masonry');
-        masonryNode.imagesLoaded(function(){
-            masonryNode.masonry({
-                itemSelector: '.item'
-            });
-        });   
+<script type="text/javascript">
+    var $container = $('#masonry');
+    $('#masonry').imagesLoaded(function(){
+        $('#masonry').masonry({
+            itemSelector: '.item',
+        });
     });
+    $('#masonry').infinitescroll({
+            loading     : {
+                msgText        : "<p>您好，已经到了最后页面。</p>",
+                img: "<?php bloginfo('template_url'); ?>/image/loading.jpg",
+                finishedMsg : "<p>文章加载中...</p>"
+            },
+            navSelector : "#nav-below",
+            nextSelector: "#nav-below #older_posts a",
+            itemSelector: "#masonry div.item",
+            extraScrollPx: 10, //滚动条距离底部多少像素的时候开始加载，默认150
+            bufferPx     : 40,//载入信息的显示时间，时间越大，载入信息显示时间越短
+            animate      : true, //当有新数据加载进来的时候，页面是否有动画效果，默认没有
+        },function(newElements) {
+    　　　　　//先隐藏
+            var $newElems = $( newElements ).css({ opacity: 0 });
+            $newElems.imagesLoaded(function(){
+    　　　　　　//图片显示后再进行masonry渲染
+              $newElems.animate({ opacity: 1 });
+              $container.masonry( 'appended', $newElems, true );
+            });
+        });  
 </script>
-
+<?php endif ?>
 <?php wp_footer(); ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -15,69 +15,17 @@
 						};
 					}else{
 						get_template_part('404'); 
-				}
+					}
 				?>
-				<div class="col-lg-wu1 col-md-3 col-sm-4 col-xs-6 item">
-					<div class="thumbnail">
-						<a href="#"><img src="https://img.zcool.cn/community/019edb5d71ddd6a801202f17f11844.jpg@260w_195h_1c_1e_1o_100sh.jpg" alt="..."></a>
-						<div class="caption">
-							<a href="1" class="title"><h3>Thumbnail label</h3></a>
-							<a href="#" class="hidden-xs"><p class="description">峻峰史丹利放计算的柯乐芙就……</p></a>
-							<p class="tags text-right"><i class="glyphicon glyphicon-time" aria-hidden="true"></i>2019-9-7 <span class="hidden-xs"><i class="glyphicon glyphicon-tags" aria-hidden="true"></i> <a href="54">博阿帕奇</a> <a href="54">标签</a></span></p>
-						</div>
-					</div>
-				</div><!-- item end -->
-				<div class="col-lg-wu1 col-md-3 col-sm-4 col-xs-6 item">
-					<div class="thumbnail">
-						<a href="#"><img src="http://image.limiwu.com/2019/2019/08/201908295462_7493.jpg" alt="..."></a>
-						<div class="caption">
-							<a href="1" class="title"><h3>Thumbnail label</h3></a>
-							<a href="#" class="hidden-xs"><p class="description">峻峰史丹利放计算的柯乐芙就……</p></a>
-							<p class="tags text-right"><i class="glyphicon glyphicon-time" aria-hidden="true"></i>2019-9-7 <span class="hidden-xs"><i class="glyphicon glyphicon-tags" aria-hidden="true"></i> <a href="54">博阿帕奇</a> <a href="54">标签</a></span></p>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-wu1 col-md-3 col-sm-4 col-xs-6 item">
-					<div class="thumbnail">
-						<a href="#"><img src="http://www.ewceo.com/zb_users/upload/2019/07/201907031562127556790452.jpg" alt="..."></a>
-						<div class="caption">
-							<a href="1" class="title"><h3>Thumbnail label</h3></a>
-							<a href="#" class="hidden-xs"><p class="description">峻峰史丹利放计算的柯乐芙就……</p></a>
-							<p class="tags text-right"><i class="glyphicon glyphicon-time" aria-hidden="true"></i>2019-9-7 <span class="hidden-xs"><i class="glyphicon glyphicon-tags" aria-hidden="true"></i> <a href="54">博阿帕奇</a> <a href="54">标签</a></span></p>
-						</div>
-					</div>
-				</div><!-- item end -->
-				<div class="col-lg-wu1 col-md-3 col-sm-4 col-xs-6 item">
-					<div class="thumbnail">
-						<a href="#"><img src="https://img.zcool.cn/community/019edb5d71ddd6a801202f17f11844.jpg@260w_195h_1c_1e_1o_100sh.jpg" alt="..."></a>
-						<div class="caption">
-							<a href="1" class="title"><h3>Thumbnail label</h3></a>
-							<a href="#" class="hidden-xs"><p class="description">峻峰史丹利放计算的柯乐芙就……</p></a>
-							<p class="tags text-right"><i class="glyphicon glyphicon-time" aria-hidden="true"></i>2019-9-7 <span class="hidden-xs"><i class="glyphicon glyphicon-tags" aria-hidden="true"></i> <a href="54">博阿帕奇</a> <a href="54">标签</a></span></p>
-						</div>
-					</div>
-				</div><!-- item end -->
-				<div class="col-lg-wu1 col-md-3 col-sm-4 col-xs-6 item">
-					<div class="thumbnail">
-						<a href="#"><img src="http://www.ewceo.com/zb_users/upload/2019/01/201901301548831064712430.jpeg" alt="..."></a>
-						<div class="caption">
-							<a href="1" class="title"><h3>Thumbnail label</h3></a>
-							<a href="#" class="hidden-xs"><p class="description">峻峰史丹利放计算的柯乐芙就……</p></a>
-							<p class="tags text-right"><i class="glyphicon glyphicon-time" aria-hidden="true"></i>2019-9-7 <span class="hidden-xs"><i class="glyphicon glyphicon-tags" aria-hidden="true"></i> <a href="54">博阿帕奇</a> <a href="54">标签</a></span></p>
-						</div>
-					</div>
-				</div><!-- item end -->
-				<div class="col-lg-wu1 col-md-3 col-sm-4 col-xs-6 item">
-					<div class="thumbnail">
-						<a href="#"><img src="http://www.ewceo.com/zb_users/upload/2019/02/201902161550292338477218.jpg" alt="..."></a>
-						<div class="caption">
-							<a href="1" class="title"><h3>Thumbnail label</h3></a>
-							<a href="#" class="hidden-xs"><p class="description">峻峰史丹利放计算的柯乐芙就……</p></a>
-							<p class="tags text-right"><i class="glyphicon glyphicon-time" aria-hidden="true"></i>2019-9-7 <span class="hidden-xs"><i class="glyphicon glyphicon-tags" aria-hidden="true"></i> <a href="54">博阿帕奇</a> <a href="54">标签</a></span></p>
-						</div>
-					</div>
-				</div><!-- item end -->
 			</div><!-- row end -->
 		</div><!-- container end -->
 	</div><!-- list end -->
+
+	<nav id="nav-below">
+	  <ul class="pager">
+	    <li><?php previous_posts_link(__('Newer posts')) ?></li>
+	    <li id="older_posts"><?php next_posts_link(__('Older posts')) ?></li>
+	  </ul>
+	</nav><!-- nav-below end -->
+
 <?php get_footer();?>
