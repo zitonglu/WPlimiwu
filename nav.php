2bb9b1b1277b700a10a9e4b97d4c4a859f2d9e10
xie <910109610@qq.com>
2019-09-27 11:31:10 +0800

    logo

--- nav.php
+++ nav.php
@@ -3,12 +3,13 @@
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only"><?php bloginfo( 'name' ); ?></span>
+        <span class="sr-only"><?php bloginfo('name'); ?></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?php bloginfo('url'); ?>"><i class="glyphicon glyphicon-home"></i> <?php bloginfo('name'); ?></a>
+      <a class="navbar-brand logoImg" href="<?php bloginfo('url'); ?>"><img alt="Brand" src="<?php bloginfo('url'); ?>/favicon.ico"></a> 
+      <a class="navbar-brand logoText" href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
     </div>
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <?php 
