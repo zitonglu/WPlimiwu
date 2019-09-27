e65bf8602318bcc0f2657a6614a56f6a0fa11669
xie <910109610@qq.com>
2019-09-27 15:50:18 +0800

    seo

--- header.php
+++ header.php
@@ -1,38 +1,38 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
+<meta charset="<?php bloginfo('charset'); ?>">
+<meta name="viewport" content="width=device-width,initial-scale=1">
 <?php wp_head(); ?>
 <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/image/favicon.ico" type="image/x-icon" />
 <!-- Bootstrap -->
-<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<!-- <link href="<?php bloginfo('template_url'); ?>/css/bootstrap.min.css" rel="stylesheet"> -->
-<link href="<?php bloginfo('template_url'); ?>/style.css" rel="stylesheet">
+<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
 
 <!--[if lt IE 9]>
-  <script src="http://apps.bdimg.com/libs/html5shiv/3.7/html5shiv.min.js"></script>
-  <script src="http://apps.bdimg.com/libs/respond.js/1.4.2/respond.min.js"></script>
+  <script src="//apps.bdimg.com/libs/html5shiv/3.7/html5shiv.min.js"></script>
+  <script src="//apps.bdimg.com/libs/respond.js/1.4.2/respond.min.js"></script>
 <![endif]-->
 
-<!-- SEO -->
-<?php if( is_single() || is_page()):?>
-<title><?php single_post_title(); ?> - <?php bloginfo('name'); ?></title>
-<meta name="description" content="<?php if (has_excerpt()) {
-        echo $description = get_the_excerpt(); //文章编辑中的摘要
-    }else {
-      //文章编辑中若无摘要，自定截取文章内容字数做为摘要
-        echo mb_strimwidth(strip_tags(apply_filters('the_content', $post->post_content)), 0, 200,"……"); 
-    } ?>">
-<?php else:?>
-<title><?php bloginfo('name'); ?></title>
-<meta name="description" content="<?php bloginfo('description'); ?>">
-<meta name="author" content="<?php the_author(); ?>">
-<?php endif ?>
+<title><?php if ( is_home() ) {
+        bloginfo('name'); echo "|"; bloginfo('description');
+    } elseif ( is_category() ) {
+        single_cat_title(); echo "|"; bloginfo('name');
+    } elseif (is_single() || is_page() ) {
+        single_post_title();
+    } elseif (is_search() ) {
+        echo "search result:"; echo "|"; bloginfo('name');
+    } elseif (is_404() ) {
+        echo '404';
+    } else {
+		wp_title( '|', true, 'right' ); 
+}?></title>
+
+<?php get_template_part( 'option-seo');//SEO ?>
+
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class();?>>
 <?php wp_body_open(); ?>
 
 <?php get_template_part( 'nav', get_post_format() );//顶部导航 ?>
---
+++ option-seo.php
@@ -0,0 +1,41 @@
+<?php 
+/**
+ * 网站SEO优化代码
+ *
+ * @package limiwuCom
+ * @author annanzi/910109610@qq.com
+ * @since 2019-9-27
+ */
+$description = '';
+$keywords = '';
+if (is_home() || is_page()) {
+    $description = get_option('paipk1_homeDescription');
+    $keywords = get_option('paipk1_homeKeyword');
+}
+elseif (is_single()) {
+   $description1 = get_post_meta($post->ID, "description", true);
+   $description2 = str_replace("\n","",mb_strimwidth(strip_tags($post->post_content), 0, 200, "…", 'utf-8'));
+   $description = $description1 ? $description1 : $description2;
+   $keywords = get_post_meta($post->ID, "keywords", true);
+   if($keywords == '') {
+      $tags = wp_get_post_tags($post->ID);
+      foreach ($tags as $tag ) {
+         $keywords = $keywords . $tag->name . ", ";
+      }
+      $keywords = rtrim($keywords, ', ');
+   }
+}
+elseif (is_category()) {
+   $description = category_description();
+   $keywords = single_cat_title('', false);
+}
+elseif (is_tag()){
+   $description = tag_description();
+   $keywords = single_tag_title('', false);
+}
+$description = trim(strip_tags($description));
+$keywords = trim(strip_tags($keywords));
+?>
+<meta name="description" content="<?php echo $description; ?>" />
+<meta name="keywords" content="<?php echo $keywords; ?>" />
+<!-- seo end -->
