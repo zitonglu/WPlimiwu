948193c0f55added8e12952668f4af922ef3742b
xie <910109610@qq.com>
2019-09-28 17:41:58 +0800

    page

--- page.php
+++ page.php
@@ -1,6 +1,6 @@
 <?php get_header();?>
-	<div class="container">
-		<div class="cow content">
+	<div class="container content">
+		<div class="cow">
 			<?php 
 				while(have_posts()){
 					the_post();
