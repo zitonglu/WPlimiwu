3eee191fdb11f91117b666308c2be447ff68fd46
my <910109610@qq.com>
2019-10-12 00:01:16 +0800

    PPT no end

--- content-top.php
+++ content-top.php
@@ -1,8 +1,9 @@
 <?php 
+if (!get_option('limiwu_top_posts')) {
 	$top_posts = get_posts("include=".get_option('limiwu_top_posts'));
 	if($top_posts){
 		$number = count($top_posts);
-		echo '<div class="jumbotron"><div class="container"><div id="myCarousel" class="carousel slide">';
+		echo '<div class="jumbotron ppt"><div class="container"><div id="myCarousel" class="carousel slide">';
 		echo '<ol class="carousel-indicators">';
 		for ($i=0; $i < $number; $i++) { 
 			echo '<li data-target="#myCarousel" data-slide-to="'.$i.'"';
@@ -17,13 +18,13 @@
 ?>
 	<div class="item media<?php echo $active;?>" id="post-<?php the_ID(); ?>">
 	  <div class="media-left">
-	    <a href="#">
-	      <img class="media-object" src="..." alt="...">
+	    <a href="<?php the_permalink(); ?>" target="_blank" class="title" title="<?php the_title_attribute(); ?>">
+	      <?php limiwu_post_first_img();?>
 	    </a>
 	  </div>
 	  <div class="media-body">
-	    <h4 class="media-heading">Media heading</h4>
-	    ...
+	    <h4 class="media-heading"><a href="<?php the_permalink(); ?>" target="_blank" class="title" title="<?php the_title_attribute(); ?>"><?php echo get_the_title(); ?></a></h4>
+	    <p><a href="<?php the_permalink(); ?>" target="_blank" class="title" title="<?php the_title_attribute(); ?>"><?php echo get_the_excerpt();?></a></p>
 	  </div>
 	</div>
 <?php }//end while?>
@@ -40,4 +41,5 @@
 		</div><!-- end myCarousel -->
 	</div><!-- end container -->
 </div><!-- end jumbotron -->
-<?php }?>
+<?php }//$top_posts end
+}?>
