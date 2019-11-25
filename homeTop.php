041c10bc427ea0e2aec2d2ce822e54f098ef6398
TBM <910109610@qq.com>
2019-11-25 18:54:13 +0800

    add

--- homeTop.php
+++ homeTop.php
@@ -1,8 +1,10 @@
 <div class="jumbotron home-top">
     <div class="col-sm-6 left">
         <h1>厘米屋空间家居设计</h1>
-        <p>...</p>
-        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+        <hr>
+        <h2> --- <span class="glyphicon glyphicon-tower"></span> --- 关于我们 ---</h2>
+        <p class="text">本网站提供效果图片收藏服务，主要服务于家装设计师、全屋定制设计师。使设计师们在与客户沟通时，可以根据客户喜好收藏精品效果图，以便视觉化的把握客户所需，从而更好的服务于客户。</p>
+        <p><a class="btn btn-default" href="#" role="button">了解更多</a> <a class="btn btn-primary" href="#" role="button">申请加入</a></p>
     </div>
     <div class="col-sm-6 hidden-xs right">
         
