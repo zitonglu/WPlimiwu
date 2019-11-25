640eea2c9ec2551de9cd96d53f47a0bee9a10b82
TBM <910109610@qq.com>
2019-11-25 15:31:48 +0800

    bg

---
+++ homeTop.php
@@ -0,0 +1,10 @@
+<div class="jumbotron home-top">
+    <div class="col-sm-6 left">
+        <h1>厘米屋空间家居设计</h1>
+        <p>...</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+    </div>
+    <div class="col-sm-6 hidden-xs right">
+        
+    </div>
+</div>
