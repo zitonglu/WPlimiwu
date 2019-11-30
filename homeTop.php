dfc1081d8842317670f4984180b906374bef1d0f
TBM <910109610@qq.com>
2019-11-30 22:57:08 +0800

    home top

--- homeTop.php
+++ homeTop.php
@@ -1,12 +1,38 @@
 <div class="jumbotron home-top">
+    <div class="page-head-social">
+        <div class="social-title text-uppercase">
+            <span>厘米屋空间</span>
+        </div>
+        <div class="page-head-social-item ul-li">
+            <ul class="page-head-social-list">
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span></a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-th-large"></span></a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-qrcode"></span></a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-picture"></span></a></li>
+            </ul><!-- /.page-head-social-list -->
+        </div><!-- /.page-head-social-item -->
+   </div>
+   <div class="container">
     <div class="col-sm-6 left">
         <h1>厘米屋空间家居设计</h1>
-        <hr>
-        <h2> --- <span class="glyphicon glyphicon-tower"></span> --- 关于我们 ---</h2>
-        <p class="text">本网站提供效果图片收藏服务，主要服务于家装设计师、全屋定制设计师。使设计师们在与客户沟通时，可以根据客户喜好收藏精品效果图，以便视觉化的把握客户所需，从而更好的服务于客户。</p>
-        <p><a class="btn btn-default" href="#" role="button">了解更多</a> <a class="btn btn-primary" href="#" role="button">申请加入</a></p>
+        <!-- <h2> --- <span class="glyphicon glyphicon-tower"></span> --- 关于我们 ---</h2>  -->
+        <p class="text">本网站提供家装效果图片收藏服务，主要服务于家装设计师、全屋定制设计师。使设计师们在与客户沟通时，可以根据客户喜好收藏精品效果图，以便视觉化的把握客户所需，从而更好的服务于客户。</p>
+        <p><a class="btn btn-default" href="#" role="button">了解更多</a><!--  <a class="btn btn-primary" href="#" role="button">申请加入</a> --></p>
+   </div>
+   <div class="col-sm-6 right">
+    <ul class="nav nav-tabs" role="tablist">
+        <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
+        <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
+        <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
+        <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li>
+    </ul>
+
+        <div class="tab-content">
+            <div role="tabpanel" class="tab-pane active" id="home">...</div>
+            <div role="tabpanel" class="tab-pane" id="profile">...</div>
+            <div role="tabpanel" class="tab-pane" id="messages">...</div>
+            <div role="tabpanel" class="tab-pane" id="settings">...</div>
+        </div>
     </div>
-    <div class="col-sm-6 hidden-xs right">
-        
-    </div>
+</div><!--container end--->
 </div>
