616a9aed9559902ef301e2db61586fa3a4c8319b
TBM <910109610@qq.com>
2020-01-03 23:15:23 +0800

    sleep

--- page-baojia.php
+++ page-baojia.php
@@ -27,8 +27,6 @@ input[type="number"]{-moz-appearance: textfield}
 /*====.footer 页面底部 ====*/
 .footer{
 	padding: 8px;
-    padding-top: 8px;
-    padding-bottom: 8px;
 	text-align: center;
 	background: #212121;
 	color: #bdbdbd;
@@ -40,11 +38,11 @@ input[type="number"]{-moz-appearance: textfield}
 .left,.table-box,.right{min-height:100vh}
 .right>.form-control,
 .right .input-group,
-.right .form-group{margin-bottom:5px}
+.right .form-group,
+.add_button button{margin-bottom:5px}
 .right>.form-control{color:#3c763d;border-color:#3c763d;}
-.right>.form-control::placeholder{color:#3c763d}
+.form-control-feedback{color:#555}
 
-/*.left,.right{min-height:100vh;background-color:#cfd8dc;color:#333}*/
 </style>
 </head>
 <body>
@@ -53,34 +51,60 @@ input[type="number"]{-moz-appearance: textfield}
 	<div class="col-sm-2 left"></div>
 	<div class="col-sm-8 table-box"></div>
 	<div class="col-sm-2 right">
-        <input type="text" class="form-control has-success" id="cab_name" name="cab_name" required tabindex="1" placeholder="柜体名称">
-        <h5>柜体大尺寸</h5>
+        <input type="text" class="form-control has-success" id="cab_name" name="cab_name" required tabindex="1" placeholder="项目(柜体/门扇)名称">
+        <h5>柜体/门扇/板材尺寸</h5>
         <!-- 柜体高度 -->
-		<div class="form-group has-success has-feedback">
+		<div class="form-group has-feedback">
           <label class="control-label sr-only" for="cab_Height">柜体高度</label>
           <div class="input-group">
-            <span class="input-group-addon">高</span>
+            <span class="input-group-addon">高(长)</span>
             <input type="number" class="form-control" id="cab_Height" name="cab_Height" required="" tabindex="1" placeholder="0">
           </div>
           <span class="form-control-feedback">mm</span>
         </div>
         <!-- 柜体宽度 -->
-        <div class="form-group has-success has-feedback">
+        <div class="form-group has-feedback">
           <label class="control-label sr-only" for="cab_Width">柜体宽度</label>
           <div class="input-group">
-            <span class="input-group-addon">宽</span>
+            <span class="input-group-addon">宽(宽)</span>
             <input type="number" class="form-control" id="cab_Width" name="cab_Width" required="" tabindex="2" placeholder="0">
           </div>
           <span class="form-control-feedback">mm</span>
         </div>
         <!-- 柜体深度 -->
-        <div class="form-group has-success has-feedback">
+        <div class="form-group has-feedback">
           <label class="control-label sr-only" for="cab_Depth">柜体深度</label>
           <div class="input-group">
-            <span class="input-group-addon">深</span>
+            <span class="input-group-addon">深(厚)</span>
             <input type="number" class="form-control" id="cab_Depth" name="cab_Depth" tabindex="3" placeholder="0">
           </div>
           <span class="form-control-feedback">mm</span>
+        </div>
+        <h5>价格</h5>
+        <!-- 面积单价 -->
+        <div class="form-group has-feedback">
+          <label class="control-label sr-only" for="sheetPrice">面积单价</label>
+          <div class="input-group"> 
+            <span class="input-group-addon">单价</span>
+            <input type="number" class="form-control" id="sheetPrice" name="sheetPrice" tabindex="6" placeholder="0.00">
+          </div> 
+          <span class="form-control-feedback"><small>元/m²</small></span>
+        </div>
+        <!-- 数量 -->
+        <div class="form-group has-feedback">
+          <label class="control-label sr-only" for="LM_number">数量</label>
+          <div class="input-group"> 
+            <span class="input-group-addon">数量</span>
+            <input type="number" class="form-control" id="LM_number" name="LM_number" tabindex="6" placeholder="1.00">
+          </div>
+        </div>
+        <h5>计算方式</h5>
+        <div class="add_button text-center">
+          <button class="btn btn-default" type="submit">增加单项</button>
+          <button class="btn btn-default" type="submit">增加单项</button>
+          <button class="btn btn-default" type="submit">增加单项</button>
+          <button class="btn btn-default" type="submit">增加单项</button>
+          <button class="btn btn-default" type="submit">增加单项</button>
         </div>
 	</div>
 	<!-- body end -->
